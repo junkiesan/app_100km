b1aac645c4a2ab68fa4bf8bbf4c27edd89590ec5
Julien Bouland <bouland.julien@gmail.com>
2020-05-25 18:33:14 +0200

    seed for venues is full

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,41 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+# require 'faker'
 require 'open-uri'
 require "json"
-require "rest-client"
+require 'rest-client'
 
-puts "Destroy Users"
-User.destroy_all
-puts "Destroy Trips"
-Trips.destroy_all
-puts "Destroy Reviews"
-Reviews.destroy_all
-puts "Destroy Venues"
-Venues.destroy_all
+# puts "Destroy Users"
+# User.destroy_all
+# puts "Destroy Trips"
+# Trips.destroy_all
+# puts "Destroy Reviews"
+# Reviews.destroy_all
+# puts "Destroy Venues"
+# Venues.destroy_all
 
-puts 'Creating user...'
-10.times do
-  users = User.new(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: "mdpmdp",
-    )
-  users.save!
-  puts 'New user!'
-end
+# puts 'Creating user...'
+# 10.times do
+#   users = User.new(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     password: "mdpmdp",
+#     )
+#   users.save!
+#   puts 'New user!'
+# end
 
 test = User.create(name: 'Nemo', email: 'nemo@100km.com', password: 'helloworld')
 
-trip = Trip.new(name: 'Hermit in the mountains 🏔', date: Date.now, user: nemo)
-truck_1.save!
+# trip = Trip.new(name: 'Hermit in the mountains 🏔', date: Date.now, user: nemo)
+# truck_1.save!
 
-response = RestClient.get "https://api.foursquare.com/v2/venues/search?ll=48.8566969,2.3514616&client_id=PKECGHIH201FYP4DKCUB4IC2RG0AEPAFICVKMBLFO1XXLNMN
-
-&client_secret=EP215GIGFV11TDBDZDZLFI1V3ZDQ2RDZWOGV5UCTGKKXIZDW
-
-&v=20202005&categoryId=4bf58dd8d48988d181941735"
+response = RestClient.get "https://api.foursquare.com/v2/venues/search?ll=48.8566969,2.3514616&client_id=PKECGHIH201FYP4DKCUB4IC2RG0AEPAFICVKMBLFO1XXLNMN&client_secret=EP215GIGFV11TDBDZDZLFI1V3ZDQ2RDZWOGV5UCTGKKXIZDW&v=20202005&categoryId=4bf58dd8d48988d181941735"
 repos = JSON.parse(response)
-
-puts repos
+repos["response"]["venues"].each do |venue|
+  Venue.create!(
+      name: venue["name"],
+      lattitude: venue["location"]["lat"],
+      longitude: venue["location"]["lng"],
+      zip: venue["postalCode"],
+      category: venue["categories"]["name"],
+      address: venue["formattedAddress"][0],
+    )
+end
