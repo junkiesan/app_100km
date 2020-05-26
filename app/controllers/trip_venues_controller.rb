a20d17ad37f2e17daeab9b08ce1cf53524ea8f89
Julien Bouland <bouland.julien@gmail.com>
2020-05-26 16:32:43 +0200

    create and destroy implemented

--- app/controllers/trip_venues_controller.rb
+++ app/controllers/trip_venues_controller.rb
@@ -1,5 +1,21 @@
 class TripVenuesController < ApplicationController
+
   def show
     @trip_venue = TripVenue.find(params[:id])
+  end
+
+  def create
+    @trip_venue = TripVenue.find(params[:id])
+    if @trip_venue.save
+      redirect_to trip_venue_path(@trip_venue)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trip_venue = TripVenue.find(params[:id])
+    @trip_venue.destroy
+    redirect_to trip_venue_path
   end
 end
