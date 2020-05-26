8c944cadae6f0f37b77759739b5408803ff1e03d
Julien Bouland <bouland.julien@gmail.com>
2020-05-26 11:16:10 +0200

    interpolation added with arrays of cities and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'faker'
-require 'open-uri'
 require "json"
 require 'rest-client'
-
-# puts "Destroy Users"
-# User.destroy_all
+puts "Destroy Users"
+User.destroy_all
 # puts "Destroy Trips"
-# Trips.destroy_all
+# Trip.destroy_all
 # puts "Destroy Reviews"
-# Reviews.destroy_all
-# puts "Destroy Venues"
-# Venues.destroy_all
-
+# Review.destroy_all
+puts "Destroy Venues"
+Venue.destroy_all
 # puts 'Creating user...'
 # 10.times do
 #   users = User.new(
@@ -29,25 +26,28 @@ require 'rest-client'
 #   users.save!
 #   puts 'New user!'
 # end
-
 test = User.create(name: 'Nemo', email: 'nemo@100km.com', password: 'helloworld')
 
-# trip = Trip.new(name: 'Hermit in the mountains 🏔', date: Date.now, user: nemo)
-# truck_1.save!
 
-response = RestClient.get "https://api.foursquare.com/v2/venues/search?ll=48.8566969,2.3514616&client_id=PKECGHIH201FYP4DKCUB4IC2RG0AEPAFICVKMBLFO1XXLNMN&client_secret=EP215GIGFV11TDBDZDZLFI1V3ZDQ2RDZWOGV5UCTGKKXIZDW&v=20202005&categoryId=4bf58dd8d48988d181941735"
-repos = JSON.parse(response)
-repos["response"]["venues"].each do |venue|
-  Venue.create!(
-      name: venue["name"],
-      lattitude: venue["location"]["lat"],
-      longitude: venue["location"]["lng"],
-      zip: venue["postalCode"],
-      category: venue["categories"]["name"],
-      address: venue["formattedAddress"][0],
-    )
+CITIES = ['Paris', 'Lyon', 'Marseille', 'Bordeaux'] # => Add Cities
+CATEGORIES = ['Musee', 'Parc', 'Plage', 'Monument'] # => Add Categories
+CITIES.each do |city|
+  puts "Fetching: #{city}"
+  CATEGORIES.each do |category|
+    puts "-- Cateogry: #{category}"
+    response = RestClient.get "https://api.foursquare.com/v2/venues/search?near=#{city},France&client_id=PKECGHIH201FYP4DKCUB4IC2RG0AEPAFICVKMBLFO1XXLNMN&client_secret=EP215GIGFV11TDBDZDZLFI1V3ZDQ2RDZWOGV5UCTGKKXIZDW&v=20202005&query=#{category}&locale=fr"
+    data = JSON.parse(response)
+    data["response"]["venues"].each do |venue|
+      if venue['categories'].any?
+        Venue.create!(
+          name: venue['name'],
+          latitude: venue['location']['lat'],
+          longitude: venue['location']['lng'],
+          zip: venue['location']['postalCode'],
+          category: venue['categories'].first['name'],
+          address: venue['location']['formattedAddress'][0]
+        )
+      end
+    end
+  end
 end
-
-
-
-
