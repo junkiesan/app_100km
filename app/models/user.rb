6401d3412511d60a8206748935b887dc2e53670f
Julien Bouland <bouland.julien@gmail.com>
2020-05-26 14:56:00 +0200

    geocoding installed and API key mapbox added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Geocoding
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   # Attractions
   has_many :trips
 
+  # Devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_091745) do
+ActiveRecord::Schema.define(version: 2020_05_26_123751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_05_26_091745) do
     t.string "address"
     t.text "bio"
     t.integer "age"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
