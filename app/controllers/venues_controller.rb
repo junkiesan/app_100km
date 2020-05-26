43f3f9dcc89b6c9c70b36e5926c55a6bc8f77a1a
Julien Bouland <bouland.julien@gmail.com>
2020-05-26 12:28:45 +0200

    venues controller added and full

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,34 @@
 class VenuesController < ApplicationController
+
+  def index
+    @venue = Venue.all
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to trips_path(@venue)
+    else
+      render :show
+    end
+  end
+
+  # def destroy
+  #   @trip.venue.destroy
+  #   redirect_to trips_path
+  # end
+
+private
+
+  def venues_params
+    params.require(:venue).permit(:name, :category, :address, :zip, :photo, :latitude, :longitude)
+  end
 end
