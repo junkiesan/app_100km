881568f91c5abe2773fa72b7ead8170620633024
Julien Bouland <bouland.julien@gmail.com>
2020-05-27 12:04:57 +0200

    application.js modified to support mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
