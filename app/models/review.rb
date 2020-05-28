ae36824300ebf3fd2f57b33e455d1097f8d28d43
Julien Bouland <bouland.julien@gmail.com>
2020-05-28 13:07:58 +0200

    controller complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,7 @@ class Review < ApplicationRecord
 # Validations
   validates :comment, :rating, presence: true
   validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+
+#ActiveStorage
+  has_many_attached :photos
 end
