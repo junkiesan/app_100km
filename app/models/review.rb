df1fcda90bd36890c26f334b98c01f9d8f01d0e1
Julien Bouland <bouland.julien@gmail.com>
2020-05-28 13:03:16 +0200

    model review complete with attractions and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
+
+# Attractions
   belongs_to :venue
   belongs_to :user
+
+# Validations
+  validates :comment, :rating, presence: true
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_155936) do
+ActiveRecord::Schema.define(version: 2020_05_28_105922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2020_05_27_155936) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["venue_id"], name: "index_reviews_on_venue_id"
   end
 
   create_table "trip_venues", force: :cascade do |t|
@@ -91,6 +102,8 @@ ActiveRecord::Schema.define(version: 2020_05_27_155936) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "venues"
   add_foreign_key "trip_venues", "trips"
   add_foreign_key "trip_venues", "venues"
   add_foreign_key "trips", "users"
