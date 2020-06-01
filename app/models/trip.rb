32bebe7a21379e8e6d58d3fe39b7b6ce2a4513d9
Kenza Mahresi <k.mahresi@gmail.com>
2020-06-01 18:11:55 +0200

    chatroom link to profile

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,13 +1,21 @@
 class Trip < ApplicationRecord
+  after_create :create_chatroom
 
 # Attractions
   belongs_to :user
   has_many :trip_venues, dependent: :destroy
   has_many :venues, through: :trip_venues
+  has_many :invitations
+  has_many :users, through: :invitations
+  has_one :chatroom
 
 
 # Geocoding
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def create_chatroom
+    self.chatroom = Chatroom.new
+  end
 
 end
