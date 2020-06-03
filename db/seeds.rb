63b7d4d59e7527e4ae0d1e3417aab59a77036c72
Kenza Mahresi <k.mahresi@gmail.com>
2020-06-03 16:29:10 +0200

    logo avatar functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,8 +46,6 @@ philippine = User.create!(name: 'Philippine Berton', email: 'user12@100km.com', 
 paul = User.create!(name: 'Paul Lahana', email: 'user13@100km.com', password: '123456')
 romain = User.create!(name: 'Romain Sanson', email: 'user14@100km.com', password: '123456')
 thomas = User.create!(name: 'Thomas Desmoulins', email: 'user15@100km.com', password: '123456')
-paul = User.create!(name: 'Paul Lahana', email: 'user16@100km.com', password: '123456')
-
 
 # CLIENT_SECRET = "DFBAQSN053VYIGQYLPTSI4ETXQ3PB1IIXUS1455EHOJEJGRQ"
 # CLIENT_ID = "5PX51VG5G0LLVYDSI0LNISENVY4WSGRZWM21ZNC3THKGQI4X"
